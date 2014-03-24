a2547995c2791cc844b8f0370cd9478a2b33a9ef
Dylan Nguyen <d_nguyen@live.com>
2014-03-24 19:06:18 -0400

    Add contacts name

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@
 	{
 		'threadid' : {
 			id: 'threadid',
+			name: 'person's name'
 			messages:[
 				{
 					id
 					address
+					name
 					message
 					type
 					time
@@ -24,7 +26,7 @@ $(document).ready(function() {
 	var ws = new WebSocket('ws://192.168.0.100:9003');
 	var threads = [];
 	ws.on('open', function() {
-	    ws.send('HI SERVER');
+	    ws.send('getMessages');
 	});
 
 	ws.on('message', function(data, flags) {
@@ -38,6 +40,7 @@ $(document).ready(function() {
 				threads[message.thread_id] = {
 					id: message.thread_id,
 					address: message.address,
+					name: message.name,
 					messages: []
 				};
 			}
@@ -45,6 +48,7 @@ $(document).ready(function() {
 			threads[message.thread_id].messages.push({
 				id: message.id,
 				address: message.address,
+				name: message.name,
 				message: message.message,
 				type: message.msgtype,
 				time: message.time
@@ -59,7 +63,11 @@ $(document).ready(function() {
 		var threadsHtml = "";
 		_.each(threads, function(thread) {
 			if (thread) {
-				threadsHtml += '<div class="thread" data-threadid="' + thread.id + '">' + thread.address + '</div>';
+				if (thread.name === '') {
+					threadsHtml += '<div class="thread" data-threadid="' + thread.id + '">' + thread.address + '</div>';
+				} else {
+					threadsHtml += '<div class="thread" data-threadid="' + thread.id + '">' + thread.name + '</div>';
+				}
 			}
 		});
 		$('.threads').append(threadsHtml);
